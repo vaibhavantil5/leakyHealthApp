4a997331b79e9cc302a5450a1911e9b9971b42e9
vaibhavantil2 <88091026+vaibhavantil2@users.noreply.github.com>
2024-07-18 10:16:31 -0400

    Update LoginActivity.java

--- app/src/main/java/io/privado/privadohealthapp/views/LoginActivity.java
+++ app/src/main/java/io/privado/privadohealthapp/views/LoginActivity.java
@@ -17,6 +17,8 @@ import com.facebook.login.widget.LoginButton;
 import com.mixpanel.android.mpmetrics.MixpanelAPI;
 import io.branch.referral.Branch;
 import io.branch.referral.BranchError;
+import com.appsflyer.AppsFlyerConversionListener;
+import com.appsflyer.AppsFlyerLib;
 import io.privado.privadohealthapp.R;
 import io.privado.privadohealthapp.models.PersonalyIndentifiableInformation;
 import io.privado.privadohealthapp.viewmodels.LoginViewModel;
@@ -41,6 +43,31 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
 
         email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+
+                // Initialize Appsflyer
+        AppsFlyerLib.getInstance().init(getString(R.string.appsflyer_dev_key), new AppsFlyerConversionListener() {
+            @Override
+            public void onConversionDataSuccess(Map<String, Object> conversionData) {
+                // Handle conversion data success
+            }
+
+            @Override
+            public void onConversionDataFail(String errorMessage) {
+                // Handle conversion data failure
+            }
+
+            @Override
+            public void onAppOpenAttribution(Map<String, String> attributionData) {
+                // Handle app open attribution
+            }
+
+            @Override
+            public void onAttributionFailure(String errorMessage) {
+                // Handle attribution failure
+            }
+        }, getApplicationContext());
+        AppsFlyerLib.getInstance().start(this);
+        
 
     }
     public void onClickLogin(View view){
